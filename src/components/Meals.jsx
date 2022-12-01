8f7da24fa59a06db6146598e6822be6b969fe92c
AndrewChacon <andrew.b.chacon@gmail.com>
2022-11-30 20:09:41 -0500

    load meals

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,9 +1,27 @@
 import { useGlobalContext } from '../context';
+import { BsHandThumbsUp } from 'react-icons/bs';
 
 const Meals = () => {
-	const context = useGlobalContext();
-	console.log(context);
-	return <h1>Meals Component</h1>;
+	const { meals } = useGlobalContext();
+	return (
+		<section className='section-center'>
+			{meals.map(singleMeal => {
+				const { idMeal, strMeal: title, strMealThumb: image } = singleMeal;
+				console.log(singleMeal);
+				return (
+					<article key={idMeal} className='single-meal'>
+						<img src={image} alt='' className='img' />
+						<footer>
+							<h5>{title}</h5>
+							<button className='like-btn'>
+								<BsHandThumbsUp />
+							</button>
+						</footer>
+					</article>
+				);
+			})}
+		</section>
+	);
 };
 
 export default Meals;
--- src/context.jsx
+++ src/context.jsx
@@ -1,12 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 
 const AppContext = React.createContext();
 
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
+// const randomMeal = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
 const AppProvider = ({ children }) => {
+	const [meals, setMeals] = useState([]);
+
+	const fetchMeals = async url => {
+		try {
+			const { data } = await axios(url);
+			setMeals(data.meals);
+		} catch (error) {
+			console.log(error.response);
+		}
+	};
+
+	useEffect(() => {
+		fetchMeals(allMealsUrl);
+	}, []);
+
 	return (
-		<AppContext.Provider value={{ name: 'Andrew', role: 'student' }}>
-			{children}
-		</AppContext.Provider>
+		<AppContext.Provider value={{ meals }}>{children}</AppContext.Provider>
 	);
 };
 
@@ -15,3 +32,7 @@ export const useGlobalContext = () => {
 };
 
 export { AppContext, AppProvider };
+
+// www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+
+// www.themealdb.com/api/json/v1/1/lookup.php?i=52772
